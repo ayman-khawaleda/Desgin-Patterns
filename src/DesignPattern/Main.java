c8bd3b89290bddf69b7e413bc01bda3776660cb5
Ayman Alkhawaleda <ayman.khawaleda@gmail.com>
2021-08-08 15:23:38 +0300

    init Node

---
+++ src/DesignPattern/Main.java
@@ -0,0 +1,8 @@
+package DesignPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World And Welcome To Design Pattern Project Based on Head First Design Pattern Book");
+    }
+}
